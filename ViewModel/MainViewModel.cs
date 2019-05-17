056f18e6b48b3222ec06e060a0fcdbbe441106d7
LÊ THANH TÂM <1612583@student.hcmus.edu.vn>
2019-05-17 14:11:07 +0700

    Interface: LoginWindow, MainWindow, UserControllBar.

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace BookStore_WPF.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public bool IsLoaded = false;
+        public ICommand LoadedWindowCommand { get; set; }
+        //Mọi thứ xử lý nằm trong này
+        public MainViewModel()
+        {
+            LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
+                IsLoaded = true;
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+            }); 
+        }
+    }
+}
