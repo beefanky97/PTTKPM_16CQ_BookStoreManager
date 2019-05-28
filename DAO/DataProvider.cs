91d6dcb414367893fe86f2b291046cdaf290e5f5
phusi <phusidcn@gmail.com>
2019-05-28 23:14:05 +0700

    Login function complete

---
+++ DAO/DataProvider.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using DTO;
+using System.Data;
+
+namespace DAO
+{
+    class DataProvider
+    {
+        private static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=W:\DeadLine\WPF\BookStoreDatabase.mdf;Integrated Security=True;Connect Timeout=30";
+
+        public static DataTable ExecuteQuerry(string querry)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            DataTable table = new DataTable();
+            using (connection)
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(querry, connection);
+                adapter.Fill(table);
+            }
+            connection.Close();
+            return table;
+        }
+
+        public static int ExecuteNonQuerry(string querry)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            int result = 0;
+            using (connection)
+            {
+                SqlCommand command = new SqlCommand(querry, connection);
+                result = command.ExecuteNonQuery();
+            }
+            connection.Close();
+            return result;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DTO;
 
 namespace BookStore_WPF
 {
