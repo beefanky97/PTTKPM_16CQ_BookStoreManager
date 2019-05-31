7dad859c15a245335cd0a74868d31279dc861bef
phusi <phusidcn@gmail.com>
2019-05-31 10:47:53 +0700

    Add some DTO

---
+++ DTO/BaoCaoCongNoDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    class BaoCaoCongNoDTO
+    {
+        private string id;
+        private string ngaylap;
+        public string Id { get => id; set => id = value; }
+        public string NgayLap { get => ngaylap; set => ngaylap = value; }
+    }
+}
---
+++ DTO/SachDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class SachDTO
+    {
+        private string id;
+        private string tensach;
+        private string tacgia;
+        private string theloai;
+        private string dongia;
+        private string luongton;
+        public string Id { get => id; set => id = value; }
+        public string TenSach { get => tensach; set => tensach = value; }
+        public string TacGia { get => tacgia; set => tacgia = value; }
+        public string TheLoai { get => theloai; set => theloai = value; }
+        public string DonGia { get => dongia; set => dongia = value; }
+        public string LuongTon { get => luongton; set => luongton = value; }
+    }
+}
