b6016587475d67225d42aa2801a2e6198100bf1f
Huynh Lam Phu Si <phusidcn@hotmail.com.vn>
2019-06-07 10:33:10 +0700

    Conflict fix

--- LoginWindow.xaml.cs
+++ LoginWindow.xaml.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -67,74 +66,3 @@ namespace BookStore_WPF
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using DTO;
-using BUS;
-
-namespace BookStore_WPF
-{
-    /// <summary>
-    /// Interaction logic for LoginWindow.xaml
-    /// </summary>
-    public partial class LoginWindow : Window
-    {
-        public LoginWindow()
-        {
-            InitializeComponent();
-        }
-
-        private void BtnCancel_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
-
-        private void BtnSignIn_Click(object sender, RoutedEventArgs e)
-        {
-            //NguoiDungDTO nguoidung = new NguoiDungDTO();
-            //nguoidung.TenDangNhap = txtUsername.Text;
-            //nguoidung.MatKhau = pswPassword.Password;
-            //if (rbCasher.IsChecked == false && rbManager.IsChecked == false && rbWarehouse.IsChecked == false)
-            //{
-            //    MessageBox.Show("Please select the type of user", "Notification", MessageBoxButton.OK, MessageBoxImage.Asterisk);
-            //}
-            //if (rbCasher.IsChecked == true)
-            //{
-            //    nguoidung.NhanVienBanHang = "1";
-            //}
-            //if (rbManager.IsChecked == true)
-            //{
-            //    nguoidung.QuanLi = "1";
-            //}
-            //if (rbWarehouse.IsChecked == true)
-            //{
-            //    nguoidung.NhanVienNhapKho = "1";
-            //}
-            //NguoiDungDTO dangnhap = NguoiDungBUS.Login(nguoidung);
-            //if (dangnhap != null)
-            //{
-            Window mainWindow = new MainWindow();
-            this.Close();
-            mainWindow.Show();
-            //}
-            //else
-            //{
-            //    MessageBox.Show("Login failed, please check user name or password","Login Error",MessageBoxButton.OK,MessageBoxImage.Stop);
-            //}
-
-        }
-    }
-}
->>>>>>> 999e4559fadc1e825bb7688827339ee53df46252
--- ReceiveMoneyWindow.xaml.cs
+++ ReceiveMoneyWindow.xaml.cs
@@ -1,33 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
-namespace BookStore_WPF
-{
-    /// <summary>
-    /// Interaction logic for ReceiveMoneyWindow.xaml
-    /// </summary>
-    public partial class ReceiveMoneyWindow : Window
-    {
-        public ReceiveMoneyWindow()
-        {
-            InitializeComponent();
-        }
-    }
-}
-=======
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -59,4 +30,3 @@ namespace BookStore_WPF
         }
     }
 }
->>>>>>> 999e4559fadc1e825bb7688827339ee53df46252
