69ec9724df8deb271b9c89a291de201c6aa40ca9
Huynh Lam Phu Si <phusidcn@hotmail.com.vn>
2019-06-11 09:56:00 +0700

    ReportBookDetail

---
+++ DTO/BaoCaoSachTonDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class BaoCaoSachTonDTO
+    {
+        private string id;
+        private string ngaylap;
+        private string soluong;
+        private List<SachDTO> sach;
+        public string Id { get => id; set => id = value; }
+        public string NgayLap { get => ngaylap; set => ngaylap = value; }
+        public string SoLuong { get => soluong; set => soluong = value; }
+        public List<SachDTO> Sach { get => sach; set => sach = value; }
+    }
+}
